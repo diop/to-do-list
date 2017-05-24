c4256cf6ce82327bbc42a676a8e139f24b1a21c0
Fodé Diop <fodesdiop@gmail.com>
2017-05-24 09:49:49 -0700

    setup routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api/todos', db.getAllTasks);
+router.get('/api/todos/:id', db.getSingleTask);
+router.post('api/todos', db.createTask);
+router.put('/api/todos', db.updatePuppy);
+router.delete('/api/todos/:id', db.removeTask);
 
 module.exports = router;
